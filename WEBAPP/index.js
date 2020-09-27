5317a942ab74ee38ea5f51172c930273a84ccdd3
dagoneth <dsgcba@hotmail.com>
2020-09-27 18:01:40 +0000

    asd

--- WEBAPP/index.js
+++ WEBAPP/index.js
@@ -1,6 +1,14 @@
-sap.ui.define([],
-	function () {
-		/*eslint-disable no-alert */
-		alert("UI5 ESTA LISTO!!");
-		/*eslint-enable no-alert */
+sap.ui.define([
+		"sap/m/Text"
+	],
+	// function () {
+	// /*eslint-disable no-alert */
+	// alert("UI5 ESTA LISTO!!");
+	// /*eslint-enable no-alert */
+	// });
+
+	function (Text) {
+		new Text({
+			text: "Hola mundo"
+		}).placeAt("content");
 	});
